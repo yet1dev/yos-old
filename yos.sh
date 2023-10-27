3941b94d6918b37590c5e5ca8263f9f27e4270b8
Emmanuel N. C. Brito <code@yet1dev.com>
2023-10-27 06:06:43 -0300

    [feat] yos >> Add Prompt Command to allow see informations easily in terminal

--- yos.sh
+++ yos.sh
@@ -6,7 +6,6 @@ YOS='/sdcard/0-yos'
 HOST='/data/data/com.termux/files'
 APPS='git openssl openssh micro zellij htop musl-dev sudo rustup'
 #==========================================
-mkdir -p ${YOS}
 
 SetupCommand(){
 	apk update
@@ -26,9 +25,23 @@ SetupCommand(){
 
 SaveCommand(){
 	cd ${HOST}
+	mkdir -p ${YOS}
 
 	rm ${YOS}/yos.tar
 	tar -cvpf ${YOS}/yos.tar home
+}
+
+PromptCommand(){
+	[ "${PWD}" != "${PWD#$HOME}" ] && apath="~${PWD#$HOME}" || apath=${PWD}
+
+	[ "${USER}" = "root" ] \
+		&& { signal=${val1:-#}; user=$(printf "\e[1;31mROOT\e[0m"); } \
+		|| { signal=${val1:-$}; user=$(printf "\e[1;32m${USER}\e[0m"); }  # Bold red / Bold green [rootless]
+
+	head=$(    printf "\e[0;36m${head}\e[0m"             ) # Normal Cyan
+	apath=$(   printf "\e[0;36m${apath}\e[0m"            ) # Normal Cyan
+
+	printf "╭─${user} in ${apath} \n╰─${signal} "
 }
 
 #==========================================
@@ -37,6 +50,7 @@ SaveCommand(){
 case $arg in
 	run)  proot-distro login alpine --termux-home -- bash;;
 	setup) SetupCommand;;
+	prompt) PromptCommand;;
 	save) SaveCommand;;
 	*)  echo 'Command not implemented yet'
 esac
