05ed49d2cfc59c508fdf0eedf74dffe0922b6821
Emmanuel N. C. Brito <code@yet1dev.com>
2023-10-27 06:06:17 -0300

    [mods] yos >> Add support to configure git in setup command

--- yos.sh
+++ yos.sh
@@ -4,13 +4,24 @@
 arg=$1
 YOS='/sdcard/0-yos'
 HOST='/data/data/com.termux/files'
-APPS='git openssl openssh micro zellij htop musl-dev'
+APPS='git openssl openssh micro zellij htop musl-dev sudo rustup'
 #==========================================
 mkdir -p ${YOS}
 
 SetupCommand(){
 	apk update
 	apk add ${APPS}
+
+# SYSTEM LOGIN
+	# read -p 'User: ' USER
+	# read -p 'Password: ' PASSWORD
+
+# GIT CONFIGURATION
+	read -p 'Name: ' NAME
+	read -p 'Email: ' EMAIL
+
+	git config --global user.name  "${NAME}"
+	git config --global user.email "${EMAIL}"
 }
 
 SaveCommand(){
