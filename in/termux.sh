6db42ae749bb5ef79cebc3eb4566efa974c73a62
Emmanuel N. C. Brito <code@yet1dev.com>
2023-10-27 05:07:47 -0300

    [feat] yos >> Add the installer for termux

---
+++ in/termux.sh
@@ -0,0 +1,21 @@
+#============================
+#      TERMUX SCRIPT
+#============================
+YOS='/sdcard/0-yos'
+HOST='/data/data/com.termux/files'
+termux-setup-storage
+
+rm -rf storage
+apt update
+apt install -y proot-distro
+
+[ -f ${YOS}/yos.tar ] && { cd ~/.. ; rm -rf home; tar -xvpf ${YOS}/yos.tar; }
+
+curl -LO yet1dev.com/yos/yos.sh
+chmod 777 yos.sh
+mv ./yos.sh ${HOST}/usr/bin/yos
+
+proot-distro install alpine
+proot-distro login alpine -- yos config
+
+termux-reload-settings
